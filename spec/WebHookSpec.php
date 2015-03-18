5f1fcb84fcdaaa51e57f627e007b8c5d6f9d0576
Loïc Mathaud <loic@mathaud.fr>
2015-03-18 19:08:27 +0100

    Refactoring (add constructor)

--- spec/WebHookSpec.php
+++ spec/WebHookSpec.php
@@ -7,18 +7,32 @@ use Prophecy\Argument;
 
 class WebHookSpec extends ObjectBehavior
 {
+    const SHOPIFY_SECRET_KEY = '8539b3b8b4aabd50084334c367b54357';
+
+    const DATA = '{"foo":"bar"}';
+    const HMAC_HEADER = '0Jn/M5TKfJl4vlnUXVd0y6GpwF6lfnXg7yqjHEJSK6U=';
+    const HMAC_HEADER_WRONG = 'Thisisawrongsecret-key!';
+
+
+    function let()
+    {
+        $this->beConstructedWith(self::SHOPIFY_SECRET_KEY);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Loicm\Shopify\WebHook');
     }
 
+    function its_contructor_need_secret_key()
+    {
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', ['']);
+    }
+
     function it_validates_signature_from_shopify_secret()
     {
-        $data = '{"foo":"bar"}';
-        $hmac_header = '0Jn/M5TKfJl4vlnUXVd0y6GpwF6lfnXg7yqjHEJSK6U=';
-        $shopify_secret = '8539b3b8b4aabd50084334c367b54357';
-
-        $this->verify($data, $hmac_header, $shopify_secret)->shouldBeBool();
-        $this->verify($data, $hmac_header, $shopify_secret)->shouldReturn(true);
+        $this->verify(self::DATA, self::HMAC_HEADER)->shouldBeBool();
+        $this->verify(self::DATA, self::HMAC_HEADER)->shouldReturn(true);
+        $this->verify(self::DATA, self::HMAC_HEADER_WRONG)->shouldReturn(false);
     }
 }
--- src/WebHook.php
+++ src/WebHook.php
@@ -4,14 +4,26 @@ namespace Loicm\Shopify;
 
 class WebHook
 {
+
     /**
-     * @param string $data
-     * @param string $hmac_header
-     * @param string $shopify_secret
+     * @param string $shopify_secret your secret key from Shopify admin panel
      */
-    public function verify($data, $hmac_header, $shopify_secret)
+    public function __construct($secret_key)
     {
-        $calculated_hmac = base64_encode(hash_hmac('sha256', $data, $shopify_secret, true));
+        if (empty(trim($secret_key))) {
+            throw new \InvalidArgumentException('Shopify secret key missing!');
+        }
+
+        $this->secret_key = $secret_key;
+    }
+
+    /**
+     * @param string $data body content of the POST request from Shopify
+     * @param string $hmac_header X-Shopify-Hmac-Sha256 HTTP header
+     */
+    public function verify($data, $hmac_header)
+    {
+        $calculated_hmac = base64_encode(hash_hmac('sha256', $data, $this->secret_key, true));
         return ($hmac_header == $calculated_hmac);
     }
 }
