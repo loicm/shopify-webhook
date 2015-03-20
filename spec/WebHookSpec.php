4daaa9c27f0a61ea96260e160c2d6476991c71db
Loïc Mathaud <loic@mathaud.fr>
2015-03-20 17:00:26 +0100

    Builtin HTTP request management

--- spec/WebHookSpec.php
+++ spec/WebHookSpec.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace spec\Loicm\Shopify;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\HeaderBag;
 
 class WebHookSpec extends ObjectBehavior
 {
@@ -24,15 +25,55 @@ class WebHookSpec extends ObjectBehavior
         $this->shouldHaveType('Loicm\Shopify\WebHook');
     }
 
-    function its_contructor_need_secret_key()
+    function its_contructor_needs_a_secret_key()
     {
         $this->shouldThrow('\InvalidArgumentException')->during('__construct', ['']);
     }
 
-    function it_validates_signature_from_shopify_secret()
+    function it_has_an_http_request_property()
     {
-        $this->verify(self::DATA, self::HMAC_HEADER)->shouldBeBool();
-        $this->verify(self::DATA, self::HMAC_HEADER)->shouldReturn(true);
-        $this->verify(self::DATA, self::HMAC_HEADER_WRONG)->shouldReturn(false);
+        $this->getHttpRequest()->shouldBeAnInstanceOf('Symfony\Component\HttpFoundation\Request');
+    }
+
+    function it_gets_data_from_http_request_body(Request $request)
+    {
+        $request->getContent()->willReturn(self::DATA)->shouldbeCalled();
+        $this->setHttpRequest($request);
+
+        $this->getDataFromRequest()->shouldBeArray();
+        $this->getDataFromRequest()->shouldReturn(['foo' => 'bar']);
+
+    }
+
+    function it_gets_signature_from_http_header(Request $request, HeaderBag $headers)
+    {
+        $headers->get('X-Shopify-Hmac-Sha256')->willReturn(self::HMAC_HEADER)->shouldbeCalled();
+        $request->headers = $headers;
+
+        $this->setHttpRequest($request);
+
+        $this->getSignatureFromRequest()->shouldReturn(self::HMAC_HEADER);
+    }
+
+    function it_validates_signature(Request $request, HeaderBag $headers)
+    {
+        $headers->get('X-Shopify-Hmac-Sha256')->willReturn(self::HMAC_HEADER)->shouldbeCalled();
+        $request->headers = $headers;
+        $request->getContent()->willReturn(self::DATA)->shouldbeCalled();
+        $this->setHttpRequest($request);
+
+        $this->verify()->shouldBeBool();
+        $this->verify()->shouldReturn(true);
+    }
+
+    function it_invalidates_signature(Request $request, HeaderBag $headers)
+    {
+        $headers->get('X-Shopify-Hmac-Sha256')->willReturn(self::HMAC_HEADER_WRONG)->shouldbeCalled();
+        $request->headers = $headers;
+        $request->getContent()->willReturn(self::DATA)->shouldbeCalled();
+        $this->setHttpRequest($request);
+
+        $this->verify()->shouldBeBool();
+        $this->verify()->shouldReturn(false);
     }
 }
--- src/WebHook.php
+++ src/WebHook.php
@@ -1,9 +1,29 @@
 <?php
 namespace Loicm\Shopify;
 
+use Symfony\Component\HttpFoundation\Request;
 
 class WebHook
 {
+    /**
+     * @var string
+     */
+    protected $secret_key;
+
+    /**
+     * @var Symfony\Component\HttpFoundation\Request
+     */
+    protected $http_request = null;
+
+    /**
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * @var string
+     */
+    protected $signature;
 
     /**
      * @param string $shopify_secret your secret key from Shopify admin panel
@@ -15,15 +35,62 @@ class WebHook
         }
 
         $this->secret_key = $secret_key;
+
+        $this->http_request = Request::createFromGlobals();
     }
 
     /**
      * @param string $data body content of the POST request from Shopify
      * @param string $hmac_header X-Shopify-Hmac-Sha256 HTTP header
+     * @return bool
      */
-    public function verify($data, $hmac_header)
+    public function verify()
     {
-        $calculated_hmac = base64_encode(hash_hmac('sha256', $data, $this->secret_key, true));
-        return ($hmac_header == $calculated_hmac);
+        $calculated_hmac = base64_encode(
+            hash_hmac(
+                'sha256',
+                json_encode($this->getDataFromRequest()),
+                $this->secret_key,
+                true
+            )
+        );
+
+        return ($calculated_hmac === $this->getSignatureFromRequest());
+    }
+
+    /**
+     * @return array data from the webhook
+     */
+    public function getDataFromRequest()
+    {
+        $this->data = json_decode($this->http_request->getContent(), true);
+
+        return $this->data;
+    }
+
+    /**
+     * @return string signature from HTTP header
+     */
+    public function getSignatureFromRequest()
+    {
+        $this->signature = $this->http_request->headers->get('X-Shopify-Hmac-Sha256');
+
+        return $this->signature;
+    }
+
+    /**
+     * @param Symfony\Component\HttpFoundation\Request
+     */
+    public function setHttpRequest(Request $request)
+    {
+        $this->http_request = $request;
+    }
+
+    /**
+     * @return Symfony\Component\HttpFoundation\Request
+     */
+    public function getHttpRequest()
+    {
+        return $this->http_request;
     }
 }
